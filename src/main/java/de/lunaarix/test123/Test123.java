8d2825cdebcec7c0403fd0101cb7c9bbdd1cddbe
Lunarix <98608343+XXLCrrafter@users.noreply.github.com>
2022-04-12 17:07:01 +0200

    Test

--- src/main/java/de/lunaarix/test123/Test123.java
+++ src/main/java/de/lunaarix/test123/Test123.java
@@ -1,13 +1,13 @@
 package de.lunaarix.test123;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Test123 extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
-
+        Bukkit.getConsoleSender().sendMessage("Moin");
     }
 
     @Override
